80f0176d4177d12a87a65d8d2cc87e0c10be1520
Isaac Lopez <isaac.t.lopez@gmail.com>
2023-06-13 12:26:30 -0600

    created tasks_controller

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,50 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.all(params[:task])
+    @task.user = current_user
+    if @task.save
+      redirect_to @task
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+    if @task.update(params_task)
+      redirect_to tasks_path(@task)
+    else
+      redirect_to edit_task_path(@task), status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def params_task
+    params.require(:task).permit(:title, :description, :status, :due_date, :type, :user_id)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+end
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,5 @@ class Task < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
   has_many :task_assigned_users
   has_many :assigned_users, through: :task_assigned_users, source: :user #this is the user's assigned tasks
+  has_one_attached :photo
 end
