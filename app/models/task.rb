2b3ce7cb6e348b00c90a010d106f5fa35a5e496e
Daniel Enrriquez <dan_enrriquez@outlook.com>
2023-06-22 16:29:45 -0600

    test

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,7 @@ class Task < ApplicationRecord
   STATUSES = ['Pending', 'Completed', 'Past Due']
   TASK_TYPES = ['Medical', 'Bills', 'Errands', 'Other']
   belongs_to :user
-  has_many :task_assigned_users
+  has_many :task_assigned_users, dependent: :destroy
   has_many :assigned_users, through: :task_assigned_users, source: :user #this is the user's assigned tasks
   has_one_attached :photo
 
