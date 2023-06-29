4d0c53f8acb8130de43f409c5a1fa9d0354c981b
Daniel Enrriquez <dan_enrriquez@outlook.com>
2023-06-29 17:50:56 -0600

    last touches

--- app/controllers/post_mentioned_users_controller.rb
+++ app/controllers/post_mentioned_users_controller.rb
@@ -4,9 +4,9 @@ class PostMentionedUsersController < ApplicationController
     @post_mentioned_user = PostMentionedUser.new(params_mentioned_users)
     @post_mentioned_user.post = post
     if @post_mentioned_user.save
-      redirect_to post_path(post), alert: " you tagged #{@post_mentioned_user.user.first_name} in a comment"
+      redirect_to posts_path, alert: " you tagged #{@post_mentioned_user.user.first_name} in a comment"
     else
-      redirect_to post_path(post), status: :unprocessable_entity
+      redirect_to posts_path, status: :unprocessable_entity
     end
   end
 
